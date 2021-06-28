e25bf945222931e3987e0b9eb533548131d5972d
Eungyeol41 <iniziato@naver.com>
2021-06-28 15:17:49 +0900

    secret

---
+++ SpMVC_006_book/src/main/java/com/callor/book/config/NaverSecret_Sample.java
@@ -0,0 +1,14 @@
+package com.callor.book.config;
+
+public class NaverSecret_Sample {
+	
+	/*
+	 * 네이버 개발자센터에 어플을 등록한 후 
+	 * CLIENT_ID, CLIENT_SECRET을 발급받아 다음 항목에 작성 후
+	 * 파일 이름을 NaverSecret로 변경 후
+	 * 프로젝트를 실행
+	 */
+	public static final String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public static final String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
+
+}
